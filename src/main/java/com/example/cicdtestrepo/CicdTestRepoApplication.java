09fb3f067943f0c3fbfbc7fe80dfc288ba33a776
swlee <swlee@wisenut.co.kr>
2025-01-10 13:58:58 +0900

    initial commit

---
+++ src/main/java/com/example/cicdtestrepo/CicdTestRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdtestrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTestRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdtestrepo/CicdTestRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdtestrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
